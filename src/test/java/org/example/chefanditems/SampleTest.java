ca121059b6ea8d54d4a673303e56e24ea1a2abf9
CharanMamidala <msmamidala123@gmail.com>
2025-05-16 11:41:49 -0400

    hey1

---
+++ src/test/java/org/example/chefanditems/SampleTest.java
@@ -0,0 +1,13 @@
+package org.example.chefanditems;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class SampleTest {
+        @Test
+        public void testAddition() {
+            int result = 2 + 3;
+            assertEquals(5, result);
+        }
+}
---
+++ src/test/java/org/example/chefanditems/test3.java
@@ -0,0 +1,7 @@
+package org.example.chefanditems;
+
+public class test3 {
+    public static void main(String[] args) {
+        System.out.println("testing 3 : created feature 3");
+    }
+}
